c7c4f175d5249838630b9730625b4a96ce76e0b1
unknown <johnnyluther96@gmail.com>
2019-12-14 11:11:40 +0530

    QR code reader changers

--- app/src/main/java/com/master/networkmanagementsystem/HomeActivity.java
+++ app/src/main/java/com/master/networkmanagementsystem/HomeActivity.java
@@ -1,16 +1,21 @@
 package com.master.networkmanagementsystem;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class HomeActivity extends AppCompatActivity {
-    Button logout;
+    Button logout,qr;
     FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
     @Override
@@ -19,6 +24,8 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         logout = findViewById(R.id.logout);
+        qr = findViewById(R.id.qr);
+        final Activity activity = this;
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +35,35 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(logouted);
             }
         });
+
+        qr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Scanning...");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(true);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Focus to the QR code", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
 
     }
 }
