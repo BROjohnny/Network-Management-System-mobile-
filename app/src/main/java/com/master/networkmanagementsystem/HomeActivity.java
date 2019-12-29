31e896a0679ef1b01f4637e7206966b4c4439213
unknown <johnnyluther96@gmail.com>
2019-12-29 13:13:08 +0530

    connect to the home page all fuctuons

--- app/src/main/java/com/master/networkmanagementsystem/HomeActivity.java
+++ app/src/main/java/com/master/networkmanagementsystem/HomeActivity.java
@@ -4,12 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
-    Button logout;
+    Button logout,qr;
     FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
     @Override
@@ -18,6 +19,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         logout = findViewById(R.id.logout);
+        qr = findViewById(R.id.qr);
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +27,15 @@ public class HomeActivity extends AppCompatActivity {
                 FirebaseAuth.getInstance().signOut();
                 Intent logouted = new Intent(HomeActivity.this, MainActivity.class);
                 startActivity(logouted);
+            }
+        });
+
+        qr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeActivity.this,"foucs to the QR",Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(HomeActivity.this,QrScanActivity.class);
+                startActivity(i);
             }
         });
 
